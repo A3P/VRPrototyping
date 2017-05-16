7d21dee25097dd7ebdc7e573b39658a1b495b5c8
marclave <marclave@uvic.ca>
2017-05-16 14:19:23 -0700

    Add Bishop piece
    Include the blender JSON file of a Bishop and extend the piece class for
    the default Bishop class to use the blender bishop as its default mesh

---
+++ source/javascripts/Chess/Pieces/Bishop.js
@@ -0,0 +1,10 @@
+import Piece from './Piece';
+
+
+class Bishop extends Piece {
+  initGenericMesh() {
+    return super.initBlenderMesh('blenderFiles/bishop.json');
+  }
+}
+
+export default Bishop;
