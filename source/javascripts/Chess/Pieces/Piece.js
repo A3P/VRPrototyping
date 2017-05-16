3833d7403b8bcec2550cc50525e4039b5fece5cd
marclave <marclave@uvic.ca>
2017-05-16 13:14:19 -0700

    Add Piece class
    A Piece is the highest level of abstraction for any Game's piece. A
    piece can move in 3D space, have a custom mesh or a blender generated
    mesh. A piece will be the basis for all types of pieces in our chess
    game

---
+++ source/javascripts/Chess/Pieces/Piece.js
@@ -0,0 +1,27 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["initBlenderMesh"] }] */
+import blendLoad from '../../Blender';
+
+class Piece {
+
+  initBlenderMesh(path) {
+    return blendLoad(path);
+  }
+
+  init(mesh, position, color = 'green') {
+    this.mesh = mesh;
+
+    this.mesh.position.set(position.x, position.y, position.z);
+    this.mesh.material.color.set(color);
+  }
+
+  getMesh() {
+    return this.mesh;
+  }
+
+  setPosition(position) {
+    this.mesh.setPosition(position.x, position.y, position.z);
+  }
+
+}
+
+export default Piece; 
