570fa2eb86b587cfcc6529090750b05fce116c56
marclave <marclave@uvic.ca>
2017-05-26 14:06:42 -0700

    Add Chess class
    Chess class is the center point of a chess game, it is initalized then
    can be invoked to play using the play() method which listens to a
    websocket for state changes. Chess has access to the scene to allow
    rendering on the fly. Has two players for keeping track of metadata.

---
+++ source/javascripts/Chess/Chess.js
@@ -0,0 +1,64 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["initGenericBlender"] }] */
+import ChessAPI from './API/ChessAPI';
+import Board from './Board';
+import loadGenericPieces from './Pieces';
+
+class Chess {
+
+  constructor(scene) {
+    this.scene = scene;
+    this.playerOne = null;
+    this.playerTwo = null;
+    this.api = new ChessAPI();
+    this.chessSet = null;
+  }
+
+  /**
+   * initGenericBlender() returns a promise that when resolved
+   * has an object of all pre-made blender chess pieces and board
+   */
+  initGenericBlender() {
+    const board = new Board();
+    const boardPromise = board.initBoard().then(() => ({ Board: board }));
+    const chessPromises = [loadGenericPieces(), boardPromise];
+    return Promise.all(chessPromises).then((chessArray) => {
+      chessArray[0].Board = chessArray[1];
+      return chessArray[0];
+    });
+  }
+
+  /**
+   * init() initializes the chess game
+   * based on the passed in chessSet object
+   */
+  init(chessSet) {
+    this.chessSet = chessSet;
+
+    this.initializePlayers();
+    this.initializeSet();
+  }
+
+  initializeSet() {
+    console.log('Set ', this.chessSet);
+    // TODO once board controller is ready we can
+    // serve the entire chessSet to it
+  }
+
+  initializePlayers() {
+    console.log('Players ', this.playerOne, this.playerTwo);
+    // TODO once server is ready we can have this for on
+    // socket connection grab player meta
+  }
+
+  play() {
+    console.log('Playing!');
+    this.api.setListenerForMessages(this.messageHandler.bind(this));
+  }
+
+  messageHandler(msg) {
+    console.log(msg);
+    console.log(this.chessSet);
+  }
+}
+
+export default Chess;
