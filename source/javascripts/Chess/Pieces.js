acdf81b1801f15cea960aeb698ac883368705b1c
marclave <marclave@uvic.ca>
2017-06-13 15:27:54 -0700

    Add make black and white pieces
    Before we only passed in the pieceSet to the boardcontroller, now we
    have the ability to pass in the pieces with the default colour of dark and light.

--- source/javascripts/Chess/Pieces.js
+++ source/javascripts/Chess/Pieces.js
@@ -1,9 +1,12 @@
+import * as THREE from 'three';
+
 import King from './Pieces/King';
 import Pawn from './Pieces/Pawn';
 import Queen from './Pieces/Queen';
 import Knight from './Pieces/Knight';
 import Bishop from './Pieces/Bishop';
 import Rook from './Pieces/Rook';
+import Piece from './Pieces/Piece';
 
 const loadGenericPieces = () => {
   const king = new King();
@@ -48,4 +51,40 @@ const loadGenericPieces = () => {
   });
 };
 
-export default loadGenericPieces;
+const makeBlackAndWhitePieces = (chessSet) => {
+  const pieceSet = {
+    King: [chessSet.King.getMesh().clone(),
+      chessSet.King.getMesh().clone()],
+    Queen: [chessSet.Queen.getMesh(),
+      chessSet.Queen.getMesh().clone()],
+    Pawn: [chessSet.Pawn.getMesh(),
+      chessSet.Pawn.getMesh().clone()],
+    Knight: [chessSet.Knight.getMesh(),
+      chessSet.Knight.getMesh().clone()],
+    Bishop: [chessSet.Bishop.getMesh(),
+      chessSet.Bishop.getMesh().clone()],
+    Rook: [chessSet.Rook.getMesh().clone(),
+      chessSet.Rook.getMesh().clone()],
+  };
+
+  Object.keys(pieceSet).forEach((el) => {
+    const whiteIndex = 0;
+    const blackIndex = 1;
+    pieceSet[el][whiteIndex].material = pieceSet[el][whiteIndex].material.clone();
+    pieceSet[el][blackIndex].material = pieceSet[el][blackIndex].material.clone();
+    const tempWhite = new Piece();
+    const tempBlack = new Piece();
+    tempWhite.init(pieceSet[el][whiteIndex], undefined, new THREE.Color('white'));
+    tempBlack.init(pieceSet[el][blackIndex], undefined, new THREE.Color('lightslategray'));
+
+    pieceSet[el][whiteIndex] = tempWhite;
+    pieceSet[el][blackIndex] = tempBlack;
+  });
+
+  return pieceSet;
+};
+
+export {
+  loadGenericPieces,
+  makeBlackAndWhitePieces
+};
